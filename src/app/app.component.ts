58ce681f9949694f02eb2d7fbd2ecc10679e71f1
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-04 12:34:30 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,38 @@
-import { Component } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Observable } from "rxjs";
-import { map, startWith } from "rxjs/operators";
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public form: FormGroup;
-  public options = ["Paris", "Nice", "Nimes"];
-  public filteredOptions: Observable<string[]>;
+  public form: FormGroup = this.fb.group({
+    input: ['', Validators.required],
+    majeur: [],
+    gender: [],
+    toggle: [],
+    select: [],
+    slider: [],
+    date: [],
+    start: [],
+    end: [],
+    autocomplete: [],
+  });
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.createForm();
-
-    this.filteredOptions = this.form.get("autocomplete").valueChanges.pipe(
-      startWith(""),
+  public options = ['Paris', 'Nice', 'Nimes'];
+  public filteredOptions: Observable<string[]> = this.form
+    .get('autocomplete')
+    .valueChanges.pipe(
+      startWith(''),
       map((value: string) =>
         this.options.filter((option: string) =>
           option.toLowerCase().includes(value.toLowerCase())
         )
       )
     );
-  }
 
-  createForm() {
-    this.form = this.fb.group({
-      input: ["", Validators.required],
-      majeur: [],
-      gender: [],
-      toggle: [],
-      select: [],
-      slider: [],
-      date: [],
-      start: [],
-      end: [],
-      autocomplete: []
-    });
-  }
+  constructor(private fb: FormBuilder) {}
 }
